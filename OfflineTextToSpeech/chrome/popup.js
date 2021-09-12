bb099f7d3dd9a8015645ce7c6be7510c97fd99e0
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-09-12 23:16:09 +0530

    This is v1.0.0 for chrome

--- OfflineTextToSpeech/chrome/popup.js
+++ OfflineTextToSpeech/chrome/popup.js
@@ -22,14 +22,14 @@ function pauseSpeech() {
     isSpeaking = false;
     speechSynthesis.pause();
     document.getElementById(
-      "pauseButton"
+      "pauseSpeechButton"
     ).innerHTML = `Resume<img class="icon-link-small"
     src="./assets/icons/resumeIcon.svg">`;
   } else {
     isSpeaking = true;
     speechSynthesis.resume();
     document.getElementById(
-      "pauseButton"
+      "pauseSpeechButton"
     ).innerHTML = `Pause<img class="icon-link-small"
     src="./assets/icons/pauseIcon.svg">`;
   }
@@ -38,60 +38,6 @@ function pauseSpeech() {
 function stopSpeech() {
   isSpeaking = false;
   speechSynthesis.cancel();
-}
-
-function changeVoice(voice) {
-  if (voice == "voice1") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[8]));
-    speech.voice = speechSynthesis.getVoices()[8];
-  } else if (voice == "voice2") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[0]));
-    speech.voice = speechSynthesis.getVoices()[0];
-  } else if (voice == "voice3") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[1]));
-    speech.voice = speechSynthesis.getVoices()[1];
-  } else if (voice == "voice4") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[11]));
-    speech.voice = speechSynthesis.getVoices()[11];
-  } else if (voice == "voice5") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[12]));
-    speech.voice = speechSynthesis.getVoices()[12];
-  } else if (voice == "voice6") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[18]));
-    speech.voice = speechSynthesis.getVoices()[18];
-  } else if (voice == "voice7") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[33]));
-    speech.voice = speechSynthesis.getVoices()[33];
-  } else if (voice == "voice8") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[37]));
-    speech.voice = speechSynthesis.getVoices()[37];
-  } else if (voice == "voice9") {
-    // console.log((speech.voice = speechSynthesis.getVoices()[41]));
-    speech.voice = speechSynthesis.getVoices()[41];
-  }
-
-  // for (let i = 0; i < 100; i++) {
-  //   console.log((speech.voice = speechSynthesis.getVoices()[i]));
-  // }
-}
-
-function changeVoiceSpeed(voiceSpeed) {
-  // For some reason, speed below 0.5 doesn't work
-  if (voiceSpeed == "speed2") {
-    speech.rate = 2;
-  } else if (voiceSpeed == "speed1.75") {
-    speech.rate = 1.75;
-  } else if (voiceSpeed == "speed1.5") {
-    speech.rate = 1.5;
-  } else if (voiceSpeed == "speed1.25") {
-    speech.rate = 1.25;
-  } else if (voiceSpeed == "speed1") {
-    speech.rate = 1;
-  } else if (voiceSpeed == "speed0.75") {
-    speech.rate = 0.75;
-  } else if (voiceSpeed == "speed0.5") {
-    speech.rate = 0.5;
-  }
 }
 
 document.getElementById("stopSpeechButton").addEventListener("click", () => {
